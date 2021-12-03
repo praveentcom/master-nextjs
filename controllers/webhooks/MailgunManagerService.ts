2e9cc78c537064f475043634713b0d5808c3bb27
Praveen Thirumurugan <mail@praveent.com>
2021-12-03 17:04:23 +0530

    Email Backup Service Webhook

--- controllers/webhooks/MailgunManagerService.ts
+++ controllers/webhooks/MailgunManagerService.ts
@@ -1,11 +1,22 @@
+import { connectToDatabase } from '../../lib/mongodb'
+
 class MailgunManagerService {
-    constructor() {}
+  constructor() {}
 
-    parseIncomingEmail(email: any) {
-        console.log(email)
-        return email;
+  async parseIncomingEmail(email: any) {
+    try {
+      var { db } = await connectToDatabase();
+      await db.collection('email-backup').insertOne({
+        email: email
+      });
+      console.log("Email from " + email?.sender + " / " + email?.subject);
+      console.log(email);
+      return true;
+    } catch (error) {
+      console.error(error);
+      return false;
     }
-
+  }
 }
 
 export default new MailgunManagerService();
