e9d278feadd085f76ec85350342ff34a7a952a4d
Praveen Thirumurugan <mail@praveent.com>
2021-12-03 14:36:51 +0530

    Add endpoint

--- api/webhooks/[id].ts
+++
@@ -1,8 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-
-const users = [""];
-
-export default (req: VercelRequest, res: VercelResponse) => {
-    const { id } = req.query;
-    res.status(200).json({ user: users[Number(id)] });
-};
